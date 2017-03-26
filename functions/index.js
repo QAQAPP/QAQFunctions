4e9562326a028432d163feb1e2fa9b1446075106
Lazy-Y <zhenyanz@usc.edu>
2017-03-26 01:39:34 -0700

    commit

--- functions/index.js
+++ functions/index.js
@@ -18,10 +18,21 @@ function sendNotification(token, payload){
 	})
 }
 
-exports.newAnswer = functions.database.ref("Questions-v1/{qid}/Users/{uid}").onWrite(event => {
+function addNotification(uid, details, qid, type){
+	const db = admin.database();
+	var newNotiRef = db.ref("Users-v1").child(uid).child("notifications").push();
+	newNotiRef.set({
+		details: details,
+		qid: qid,
+		type: type
+	});
+}
+
+exports.questionAnswered = functions.database.ref("Questions-v1/{qid}/Users/{uid}").onWrite(event => {
 	var db = admin.database();
 	db.ref("Questions-v1").child(event.params.qid).child("content").child("askerID").once("value", function(snapshot){
-		var ref = db.ref("Users-v1").child(snapshot.val()).child("info").child("FCM Token");
+		const uid = snapshot.val();
+		var ref = db.ref("Users-v1").child(uid).child("info").child("FCM Token");
 		ref.once("value", function(snapshot) {
 			const token = snapshot.val();
 			var ref = db.ref("Users-v1").child(event.params.uid).child("info").child("username").once("value", function(snapshot){
@@ -32,15 +43,19 @@ exports.newAnswer = functions.database.ref("Questions-v1/{qid}/Users/{uid}").onW
 				    notification : {
 				      body : bodyText
 				      // title : "Question answered!"
+				    },
+				    data :{
+				    	qid : event.params.qid
 				    }
 				};
+				addNotification(uid, event.params.uid, event.params.qid, "questionAnswered");
 				sendNotification(token, payload);
 			})
 		});
 	})
 });
 
-exports.numAnswer = functions.database.ref("Questions-v1/{qid}/content/val").onWrite(event => {
+exports.questionViewed = functions.database.ref("Questions-v1/{qid}/content/val").onWrite(event => {
 	var val = event.data.val();
 	if (val > 0 && val%5 == 0){
 		var copy = val;
@@ -54,9 +69,13 @@ exports.numAnswer = functions.database.ref("Questions-v1/{qid}/content/val").onW
 					var payload = {
 					    notification : {
 					      body : bodyText
+					    },
+					    data :{
+					    	qid : event.params.qid
 					    }
 					};
 					const token = snapshot.val();
+					addNotification(uid, val, event.params.qid, "questionViewed");
 					sendNotification(token, payload);
 				});
 			})
@@ -65,7 +84,7 @@ exports.numAnswer = functions.database.ref("Questions-v1/{qid}/content/val").onW
 });
 
 
-exports.questionConclude = functions.database.ref("Questions-v1/{qid}/content/conclusion").onWrite(event => {
+exports.questionConcluded = functions.database.ref("Questions-v1/{qid}/content/conclusion").onWrite(event => {
 	var oid = event.data.val();
 	if (oid != "nil"){
 		const db = admin.database();
@@ -78,15 +97,19 @@ exports.questionConclude = functions.database.ref("Questions-v1/{qid}/content/co
 				const ref = db.ref("Questions-v1").child(event.params.qid).child("content").child("askerID")
 				ref.once("value", function(snapshot){
 					const askerID = snapshot.val();
-					const ref = db.ref("Users-v1").child(uid).child("info").child("username");
+					const ref = db.ref("Users-v1").child(askerID).child("info").child("username");
 					ref.once("value", function(snapshot){
 						const user = snapshot.val();
 						const bodyText = "Congratulations! " + user + " accepted your answer.";
 						payload = {
 						    notification : {
 						      body : bodyText
+						    },
+						    data :{
+						    	qid : event.params.qid
 						    }
 						};
+						addNotification(uid, askerID, event.params.qid, "questionConcluded");
 						sendNotification(token, payload);
 					});
 				});
